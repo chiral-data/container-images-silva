3596b488a8d2be940bc300be44dd85ec8cfc4fc7
Qin Wan <rogerwq@gmai.com>
2025-09-05 07:05:40 +0000

    add: gromacs image

--- build.sh
+++ build.sh
@@ -15,19 +15,24 @@ for DIR in $SUBFOLDERS; do
         continue
     }
 
-    # Run the command
     app_name=$(basename "$PWD")
     image_name=${app_name}_${today}
-    echo "Building container image $image_name"
-    echo ""
-    docker build -t ${image_name} --platform linux/amd64 .
-    docker run --gpus all ${image_name}
+    image_name_remote=${REGISTRY_URL}/${app_name}:${today}
 
-    echo "Tag container image $image_name"
-    docker tag ${image_name} ${REGISTRY_URL}/${app_name}:${today}
+    if docker manifest inspect "$image_name_remote" >/dev/null 2>&1; then
+        echo "Container image $image_name_remote exist ... Skip build"
+    else
+        echo "Building container image $image_name"
+        echo ""
+        docker build -t ${image_name} --platform linux/amd64 .
+        docker run --gpus all ${image_name}
 
-    echo "Push container image $image_name"
-    docker push ${REGISTRY_URL}/${app_name}:${today}
+        echo "Tag container image $image_name"
+        docker tag ${image_name} ${image_name_remote}
+
+        echo "Push container image $image_name"
+        docker push ${REGISTRY_URL}/${app_name}:${today}
+    fi
 
     # Return to the original base directory
     cd - >/dev/null
